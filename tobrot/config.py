27e7e230612d4da742b5a11686f344fb0d7de82f
vpn12 <70795634+vpn12@users.noreply.github.com>
2021-02-04 19:33:17 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,20 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1605318844:AAFjuuOINr4eldGvCCc1E0WY1DKTPqPebUY" #imp
+    APP_ID = 2572948 #imp
+    API_HASH = "3a6290dba92fd085c06ef07ba29ed52f" #imp
+    AUTH_CHANNEL = [-424731631, 1113008502] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://index.asdf1.workers.dev"
+    GLEECH_COMMAND = "gleech@torrent2gdriveasdf_bot"
+    YTDL_COMMAND = 'ytdl@torrent2gdriveasdf_bot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@torrent2gdriveasdf_bot"
+    CLONE_COMMAND_G = "gclone@torrent2gdriveasdf_bot"
+    PYTDL_COMMAND_G = "pytdl@torrent2gdriveasdf_bot"
+    DESTINATION_FOLDER = "Torrent-Gdriveasdf"
+    LEECH_COMMAND = "leech@torrent2gdriveasdf_bot"
+    CANCEL_COMMAND_G = "cancel@torrent2gdriveasdf_bot"
+    RCLONE_CONFIG = """"
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
